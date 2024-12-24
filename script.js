1078c31b711ae1d68e369c3e8b259138ba60f171
justine-tech <justinepoito12@gmail.com>
2024-12-24 20:16:06 +0800

    Add files via upload
    okguys

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("messages").onclick=function(){
+    document.getElementById("ha").innerHTML = "<p style='padding-bottom: 40px; padding-top: 50px; padding-left: 20px margin:40px;'>\u2B50 Happy Birthday \u2B50 <br><br>\u2764\uFE0F  Ninang Kris! \u2764\uFE0F </p>";
+    let hp = document.getElementById("hp");
+    document.getElementById("picc").style.display = 'block';
+    hp.play();
+}
